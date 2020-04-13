b0872fdf9111a6d946e4fadaf5aab455232e6bf2
Moe Abdelaziz <moe.abdelaziz@gmail.com>
2020-04-12 21:46:42 -0700

    Dont start a snapshot if there is already one pending

--- analyser/analyser.py
+++ analyser/analyser.py
@@ -19,6 +19,12 @@ def filter_instances(project):
 
     return instances
 
+## Check snapshot status and call it before your create the snapshot
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
+
 @click.group()
 def cli():
         """Manage snapshots"""
@@ -103,6 +109,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
         
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(" Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+            
             print("   Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Analyser user")
         
